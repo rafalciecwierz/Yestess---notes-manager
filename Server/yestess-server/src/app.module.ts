93f78a81f4fb6143f97dcae07f0266ef9637d907
Rafał C <rafal.ciecwierz4@gmail.com>
2021-01-07 23:06:07 +0100

    FIX: Lint formater is working wrong :-1:

--- Server/yestess-server/src/app.module.ts
+++ Server/yestess-server/src/app.module.ts
@@ -1,22 +1,27 @@
 import 'dotenv/config';
-import { Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    "type": "postgres",
-    "host": process.env.DB_HOST,
-    "port": parseInt(process.env.DB_PORT),
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "synchronize": true,
-    "logging": true,
-    "entities": [join(__dirname, '**', `*.entity.{ts,js}`),"dist/**/*.entity{.ts,.js}"]
-  })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+      entities: [
+        join(__dirname, '**', `*.entity.{ts,js}`),
+        'dist/**/*.entity{.ts,.js}',
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
